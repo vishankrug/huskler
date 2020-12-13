65693a29af8687aa0f34e18b03f43083cd293226
Vishank Rughwani <vishankr@uw.edu>
2020-12-13 11:14:11 +0400

    started search function

--- src/search.js
+++ src/search.js
@@ -1,42 +1,53 @@
 import React from 'react';
 import './App.css';
 import { Button } from 'reactstrap';
+import { useState } from 'react';
 
 
 export function SearchBarEvent(){
 
+    
+
     return(
         <form className="form">
-        <label className="form-child">
             Event Name: <input type="text" name="event-name" />
-        </label>
-        <label className="form-child">
             Hosted By: <input type="text" name="hosted-by" />
-        </label>
-        <Button className="button" color="primary">Submit!</Button>
-    </form>
+            <Button className="button" color="primary">Submit!</Button>
+        </form>
     )
 }
 
 
 export function SearchBarPage(){
-    
+
+    const [nameState, setNameSearch] = useState('John Doe');
+
+    const [majorState, setMajorSearch] = useState('Informatics');
+
+    const [interestsState, setInterestsSearch] = useState('Cycling');
+
+    const updateNameSearch = (event) => {
+        console.log(event.target.value);
+        setNameSearch(event.target.value);
+    }
+
+    const updateMajorSearch = (event) => {
+        console.log(event.target.value);
+        setMajorSearch(event.target.value);
+    }
+
+    const updateInterestsSearch = (event) => {
+        console.log(event.target.value);
+        setInterestsSearch(event.target.value);
+    }
+
     return(
 
     <form className="form">
-        <label className="form-child">
-            Name: <input type="text" name="name" />
-        </label>
-        <label className="form-child">
-            Major: <input type="text" name="major" />
-        </label>
-        <label className="form-child">
-            Interest: <input type="text" name="interests" />
-        </label>
+        Name: <input type="text" name="name" value={nameState} onChange={updateNameSearch.bind()}/>
+        Major: <input type="text" name="major" value={majorState} onChange={updateMajorSearch.bind()}/>
+        Interest: <input type="text" name="interests" value={interestsState} onChange={updateInterestsSearch.bind()}/>
         <Button className="button"color="primary">Submit!</Button>
     </form>
-
-      
-
     )
   }
